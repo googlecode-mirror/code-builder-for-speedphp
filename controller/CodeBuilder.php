416008ed61d9b63f894d330f93d210d7a2b39a60
flymwy <flymwy@gmail.com>
2013-01-05 03:56:57 +0000

    1.模型生成器完成 2.控制器生成器建立完成

--- controller/CodeBuilder.php
+++ controller/CodeBuilder.php
@@ -21,6 +21,61 @@ class CodeBuilder extends spController{
                 $this -> display('codeBuilder/index.html');
         }
         
+	public function genController(){
+		$this ->display('codeBuilder/genController.html');
+	}
+	
+	public function genControllerAction(){
+		$actionType = $this->spArgs('genAction', 'preview');
+		$this -> tpl_controllerName = $controllerName = $this -> spArgs('controllerName','null');
+		$genCode = <<<EOT
+<?php
+class {$controllerName} extends spController{
+	public function {$GLOBALS['G_SP']['default_action']}(){
+		\$this -> display('{$controllerName}/{$GLOBALS['G_SP']['default_action']}.html');
+	}
+}
+EOT;
+		$genTemplate = array();
+		$genTemplate[0] = array('path'=>$GLOBALS['G_SP']['view']['config']['template_dir'].'/'.$controllerName.'/'.$GLOBALS['G_SP']['default_action'].'.html');
+		$genTemplate[0]['contents']  = <<<EOT
+<!DOCTYPE html>
+<html>
+        <head>
+                <title>Hello</title>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        </head>
+	<body>
+		Hello, World!
+	</body>
+</html>
+EOT;
+		if($actionType == 'preview'){
+			$this -> tpl_preview = true;
+			$this -> tpl_codePath = $GLOBALS['G_SP']['controller_path'].'/'.$controllerName.'.php';
+			$this -> tpl_codePreview = $genCode;
+			$this -> tpl_genTemplatePreview = $genTemplate;
+			$this -> genController();
+		}
+		if($actionType == 'gen'){
+			$this -> tpl_gen = true;
+			$this -> tpl_codePath = $GLOBALS['G_SP']['controller_path'].'/'.$controllerName.'.php';
+			if(!preg_match('/[0-9a-zA-Z]/', $controllerName))
+				$this -> tpl_success = -1;
+			else{
+				$this -> tpl_success = file_put_contents($GLOBALS['G_SP']['controller_path'].'/'.$controllerName.'.php', $genCode);
+				foreach($genTemplate as $template){
+					if(!is_file($GLOBALS['G_SP']['view']['config']['template_dir'].'/'.$controllerName.'/')){
+						mkdir($GLOBALS['G_SP']['view']['config']['template_dir'].'/'.$controllerName.'/');
+					}
+					$successTemplate = file_put_contents($template['path'], $template['contents']);
+					if(empty($successTemplate)) $this->tpl_success_template = -1;
+				}
+			}
+			$this -> genController();
+		}
+	}
+	
         public function genModel(){
                 $model = new spModel();
                 $this -> tpl_prefix = $GLOBALS['G_SP']["db"]['prefix'];
@@ -30,9 +85,11 @@ class CodeBuilder extends spController{
 	
 	public function genModelAction(){
 		$actionType = $this->spArgs('genAction', 'preview');
-		$modelName = $this->spArgs('modelName');
-		$modelTable = $this->spArgs('modelTable');
-		$tableColInfo = spClass('spModel') -> findSql('show columns from '.$modelTable.';');
+		$this -> tpl_modelName = $modelName = $this->spArgs('modelName');
+		$modelTable = preg_replace('/'.$this->spArgs('modelTablePrefix').'/', '', $this->spArgs('modelTable'), 1);//表名不含前缀
+		$this -> tpl_modelTable = $realTable = $this->spArgs('modelTable');//表名含前缀
+		$this -> tpl_modelTablePrefix = $this->spArgs('modelTablePrefix');
+		$tableColInfo = spClass('spModel') -> findSql('show columns from '.$realTable.';');
 		$tablePk = $this -> _findPK($tableColInfo);
 		$genCode = <<<EOT
 <?php
@@ -47,10 +104,19 @@ class {$modelName} extends spModel{
 }
 EOT;
 		if($actionType == 'preview'){
-			echo $genCode;
+			$this -> tpl_preview = true;
+			$this -> tpl_codePath = $GLOBALS['G_SP']['model_path'].'/'.$modelName.'.php';
+			$this -> tpl_codePreview = $genCode;
+			$this -> genModel();
 		}
 		if($actionType == 'gen'){
-			
+			if(!preg_match('/[0-9a-zA-Z]/', $modelName))
+				$this -> tpl_success = -1;
+			else
+				$this -> tpl_success = file_put_contents($GLOBALS['G_SP']['model_path'].'/'.$modelName.'.php', $genCode);
+			$this -> tpl_gen = true;
+			$this -> tpl_codePath = $GLOBALS['G_SP']['model_path'].'/'.$modelName.'.php';
+			$this -> genModel();
 		}
 	}
         
